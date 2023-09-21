bccc5a05dbbe7495de46adf170b6b515ce5af743
petrov6525 <petrov6525@gmail.com>
2023-09-21 21:45:16 +0300

    add logo update

--- src/main/java/com/lingohub/restfull/RestfullApplication.java
+++ src/main/java/com/lingohub/restfull/RestfullApplication.java
@@ -22,7 +22,7 @@ public class RestfullApplication {
 		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin",
 				"Content-Type", "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+				"Access-Control-Request-Method", "Access-Control-Request-Headers", "auth-token"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept",
 				"Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
