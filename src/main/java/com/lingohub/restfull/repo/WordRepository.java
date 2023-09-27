4c1c63e380a69902b71f6f1676510ca30850df57
petrov6525 <petrov6525@gmail.com>
2023-09-27 22:50:44 +0300

    add get all words add get word collection

--- src/main/java/com/lingohub/restfull/repo/WordRepository.java
+++ src/main/java/com/lingohub/restfull/repo/WordRepository.java
@@ -3,6 +3,9 @@ package com.lingohub.restfull.repo;
 import com.lingohub.restfull.models.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface WordRepository extends JpaRepository<Word, Long> {
 
+    Collection<? extends Word> findAllByDictionaryId(long id);
 }
--- src/main/java/com/lingohub/restfull/service/WordService.java
+++ src/main/java/com/lingohub/restfull/service/WordService.java
@@ -6,6 +6,10 @@ import com.lingohub.restfull.repo.WordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 @Service
 public class WordService {
     private final WordRepository wordRepository;
@@ -28,5 +32,17 @@ public class WordService {
     public Word addToDictionary(Word word) {
         word.setDictionary(dictionaryService.findById(word.getDictionary().getId()));
         return wordRepository.save(word);
+    }
+
+    public Object getCollection(Long[] dictionaries) {
+        List<Word> words = new ArrayList<>();
+        for (long id : dictionaries) {
+            words.addAll(wordRepository.findAllByDictionaryId(id));
+        }
+        return words;
+    }
+
+    public Collection<? extends Word> findAllByDictionaryId(long dictionaryId) {
+        return wordRepository.findAllByDictionaryId(dictionaryId);
     }
 }
