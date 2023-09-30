80c3a35c856ed0d1448d42f1bdbfeb168c7dcba6
YehorBelenkov <yehorbelenkov@gmail.com>
2023-09-30 11:03:01 -0500

    FUck Yeah changes!

--- Frontend-main/src/app/app.component.ts
+++ Frontend-main/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from "./auth.service";
 import { User } from "./models/user.interface";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,12 @@ export class AppComponent implements OnInit {
   currentUser: User | null = null; // Initialize currentUser to null
   email: string = '';
   password: string = '';
+  username: string = '';
   constructor(
     private router: Router,
     private authService: AuthService,
     private cdr: ChangeDetectorRef, // Inject ChangeDetectorRef
+    private http: HttpClient,
 
   ) {}
   login(): void {
@@ -33,7 +36,35 @@ export class AppComponent implements OnInit {
     // Fetch user data from local storage
     this.currentUser = this.authService.getCurrentUser();
   }
+  register() {
+    // Create the user object to send to the server
+    const user = {
+      name: this.username,
+      email: this.email,
+      password: this.password,
+    };
 
+    // Define headers with the authentication token
+    const headers = new HttpHeaders({
+      'auth-token': 'sirh545dff4e5f4ffkfjhe',
+    });
+
+    // Send a POST request to add the user to the database
+    this.http
+      .post('http://localhost:8081/users/add', user, { headers })
+      .subscribe(
+        (response) => {
+          // Registration successful, you can redirect or display a success message
+          console.log('Registration successful:', response);
+          // Redirect to login page
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          // Handle registration error, display an error message or log the error
+          console.error('Registration error:', error);
+        }
+      );
+  }
   isRootRoute(): boolean {
     return this.router.url === '/';
   }
