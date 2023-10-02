457f8494c5a3882b79fd06066ff793bc45044e25
YehorBelenkov <yehorbelenkov@gmail.com>
2023-10-02 02:53:12 -0500

    FUck Yeah changes!12

--- Frontend-main/src/app/dictionaries/dictionaries.component.ts
+++ Frontend-main/src/app/dictionaries/dictionaries.component.ts
@@ -1,16 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-dictionaries',
   templateUrl: './dictionaries.component.html',
   styleUrls: ['./dictionaries.component.css']
 })
-export class DictionariesComponent {
+export class DictionariesComponent implements OnInit {
+  currentUser: any;
+  dictionaries: any[] = []; // Declare and initialize the 'dictionaries' array
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.currentUser = this.authService.getCurrentUser();
+    const userId = this.currentUser.id;
+    const authToken = 'sirh545dff4e5f4ffkfjhe';
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'auth-token': authToken
+    });
+
+    this.http.get(`http://localhost:8081/dictionary/all/${userId}`, { headers }).subscribe(
+      (data: any) => {
+        // Handle the data received from the API
+        this.dictionaries = data; // Assign the fetched data to the 'dictionaries' array
+      },
+      (error) => {
+        // Handle any errors
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
 
   redirectToDictionary(dictionaryName: string) {
-    this.router.navigate(['/dictionaries', dictionaryName]);
+    // Replace spaces with underscores in the dictionaryName
+    const formattedDictionaryName = dictionaryName.replace(/\s+/g, '_');
+
+    // Navigate to the formatted dictionary name
+    this.router.navigate(['/dictionaries', formattedDictionaryName]);
   }
 }
